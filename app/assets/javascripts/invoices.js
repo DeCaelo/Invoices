91c85ff8885db3c6966beb789f83608ed7049e25
Ludovic Cleuet <ludovic.cleuet@gmail.com>
2016-09-04 17:41:19 +0200

    event for total subtotal added

--- app/assets/javascripts/invoices.js
+++ app/assets/javascripts/invoices.js
@@ -1,3 +1,32 @@
-# Place all the behaviors and hooks related to the matching controller here.
-# All this logic will automatically be available in application.js.
-# You can use CoffeeScript in this file: http://coffeescript.org/
+function update_price() {
+  var row = $(this).parents('.item-row');
+  var price = row.find('.cost').val() * row.find('.qty').val();
+  price = price.toFixed(2);
+  isNaN(price) ? row.find('.price').html('NaN') : row.find('.price').html(price);
+  update_subtotal();
+}
+
+function bind1() {
+  $('.cost').blur(update_price);
+  $('.qty').blur(update_price);
+}
+
+function update_subtotal() {
+  var subtotal = 0;
+  $('.price').each(function(i){
+    var price = $(this).html();
+    if (!isNaN(price)) subtotal += Number(price);
+  });
+
+  subtotal = subtotal.toFixed(2);
+  $('#subtotal').html(subtotal);
+
+  update_balance();
+}
+
+function update_balance() {
+  var total = Number($('#subtotal').html()) + Number($('#9').val());
+  total = total.toFixed(2);
+
+  $('.due').html(total);
+}
